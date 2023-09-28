0b031ab68e4e857d635444f724521be1d660efcb
seif2k1 <seif2kghost@gmail.com>
2023-09-28 04:55:59 +0200

    new

--- src/skills/ListSkill.js
+++ src/skills/ListSkill.js
@@ -1,31 +1,54 @@
+import javaScript from "../images/js.png";
+import html5 from "../images/html-5.png";
+import css3 from "../images/css-3.png";
+import bootstrap from "../images/bootstrap.png";
+import tailwind from "../images/icons8-tailwind-css-100.png";
+import git from "../images/git.png";
+import gitHub from "../images/github.png";
+import reactjs from "../images/structure.png";
+import nextjs from "../images/nextjs (1).svg";
+import reduxToolkit from "../images/icons8-redux-an-open-source-javascript-library-for-managing-application-state-24.png";
 export const ListSkill = [
   {
     name: "Java Scrpit",
-    value: "80%",
+    img: javaScript,
   },
   {
     name: "HTML",
-    value: "90%",
+    img: html5,
   },
   {
     name: "CSS",
-    value: "90%",
-  },
-  {
-    name: "Git/Git-Hub",
-    value: "75%",
-  },
-];
-export const Framworks = [
-  {
-    name: "React.js",
+    img: css3,
   },
   {
     name: "Bootstrap",
+    img: bootstrap,
   },
-];
-export const Bonus = [
   {
-    name: "Redux-Toolkit",
+    name: "Tailwind",
+    img: tailwind,
+  },
+
+  {
+    name: "React",
+    img: reactjs,
+  },
+  {
+    name: "React",
+    img: nextjs,
+  },
+
+  {
+    name: "Git",
+    img: reduxToolkit,
+  },
+  {
+    name: "Git-Hub",
+    img: gitHub,
+  },
+  {
+    name: "Git",
+    img: git,
   },
 ];
--- src/skills/Skill.jsx
+++ src/skills/Skill.jsx
@@ -1,89 +1,73 @@
-import React from 'react'
+import React from "react";
 import img from "../images/Group.png";
-import './Skills.css';
-import { Bonus, Framworks, ListSkill } from './ListSkill';
-import {motion} from "framer-motion";
+import "./Skills.css";
+import { Bonus, Framworks, ListSkill } from "./ListSkill";
+import { motion } from "framer-motion";
+import { BiLogoJavascript } from "react-icons/bi";
 
 const Skills = () => {
   return (
-    <div className='skills' id="skills">
-        <motion.div
-        className='group-logo'>
-            <img src={img} alt="img" />
-        </motion.div>
-        <motion.div 
-        className='skills-descreption'>
-            <motion.h1 
-            initial={{x:'-50px',opacity:0}}
-            whileInView={{x:'0px',opacity:1}}
-            transition={{duration:3,type:"spring",stiffness:50}}
-            className="My-Skills">
-                My Skills
-                <div className=''></div>
-            </motion.h1>
-            {ListSkill.map((skill,index)=>{
-                return(
-                    <div className="skills-details" key={index}>
-                        <motion.h1 
-                            initial={{x:'-50px',opacity:0}}
-                            whileInView={{x:'0px',opacity:1}}
-                            transition={{duration:3,type:"spring",stiffness:50}}
-                        > {skill.name} </motion.h1> 
-                        <motion.div 
-                            initial={{x:'-50px',opacity:0}}
-                            whileInView={{x:'0px',opacity:1}}
-                            transition={{duration:3,type:"spring",stiffness:50}}
-                        className="progress" role="progressbar" aria-label="Animated striped example" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100">
-                            <div className="progress-bar progress-bar-striped progress-bar-animated " style={{width: skill.value}}>{skill.value}</div>
-                        </motion.div>
-                    </div>
-                )
-            })}
-            <motion.h1 
-            initial={{x:'-50px',opacity:0}}
-            whileInView={{x:'0px',opacity:1}}
-            transition={{duration:3,type:"spring",stiffness:50}}
-            className="Framworks">
-                Framworks
-                <div className=''></div>
-            </motion.h1>
-                <div className='skills-framworks'>
-                    {Framworks.map((fram,index)=>{
-                        return(
-                            <div className='framworks' key={index}>
-                                <motion.h1
-                            initial={{x:'-50px',opacity:0}}
-                            whileInView={{x:'0px',opacity:1}}
-                            transition={{duration:3,type:"spring",stiffness:50}}
-                                > {fram.name} </motion.h1> 
-                            </div>
-                        )
-                    })}
-                </div>
-            <motion.h1 
-            initial={{x:'-50px',opacity:0}}
-            whileInView={{x:'0px',opacity:1}}
-            transition={{duration:3,type:"spring",stiffness:50}}
-            className="Bonus">
-                Bonus
-                <div className=''></div>
-            </motion.h1>
-                <div className='skills-bonnus'>
-                    {Bonus.map((bonus,index)=>{
-                        return(
-                            <div className='framworks' key={index}>
-                                <motion.h1
-                            initial={{x:'-50px',opacity:0}}
-                            whileInView={{x:'0px',opacity:1}}
-                            transition={{duration:3,type:"spring",stiffness:50}}
-                                > {bonus.name} </motion.h1> 
-                            </div>
-                        )
-                    })}
-                </div>
-        </motion.div>
-</div>
-  )
-}
+    <div className="skills " id="skills">
+      <motion.div className="group-logo">
+        <img src={img} alt="img" />
+      </motion.div>
+      <motion.div
+        className="skills-descreption"
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          flexWrap: "wrap",
+          height: "30%",
+        }}
+      >
+        <motion.h1
+          initial={{ x: "-50px", opacity: 0 }}
+          whileInView={{ x: "0px", opacity: 1 }}
+          transition={{ duration: 3, type: "spring", stiffness: 50 }}
+          className="My-Skills"
+        >
+          My Skills
+        </motion.h1>
+        <div
+          style={{
+            width: "100%",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "space-between",
+            flexWrap: "wrap",
+            gap: "43px",
+          }}
+        >
+          {ListSkill.map((skill, index) => {
+            return (
+              <div className="skills-details" key={index}>
+                <motion.div
+                  initial={{ x: "-50px", opacity: 0 }}
+                  whileInView={{ x: "0px", opacity: 1 }}
+                  transition={{ duration: 3, type: "spring", stiffness: 50 }}
+                  style={{
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "space-between",
+                    flexWrap: "wrap",
+                    width: "100%",
+                  }}
+                >
+                  <img
+                    src={skill.img}
+                    alt={skill.name}
+                    height={50}
+                    width={50}
+                  />
+                </motion.div>
+              </div>
+            );
+          })}
+        </div>
+      </motion.div>
+    </div>
+  );
+};
 
-export default Skills
+export default Skills;
